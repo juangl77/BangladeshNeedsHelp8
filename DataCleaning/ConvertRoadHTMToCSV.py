8a1c6fe098a0b5020011ea3c01ec547772a1076c
eebart <eebartholomew@gmail.com>
2017-02-20 16:50:13 +0100

    Add road id to data, clean up printing, quotes

--- DataCleaning/ConvertRoadHTMToCSV.py
+++ DataCleaning/ConvertRoadHTMToCSV.py
@@ -5,15 +5,16 @@ import re
 from itertools import chain
 
 class RoadEntry():
-    lrpNumber = ""
+    roadId = ''
+    lrpNumber = ''
     roadChainage = -1
-    lrpType = ""
-    desc = ""
+    lrpType = ''
+    desc = ''
     latitude = -1
     longitude = -1
 
-roadRoot = ".lrps.htm"
-directory = "../RMMS/"
+roadRoot = '.lrps.htm'
+directory = '../RMMS/'
 
 if not os.path.exists(directory+'CSV/'):
     os.makedirs(directory+'CSV/')
@@ -22,19 +23,21 @@ allRoads = []
 for filename in os.listdir(directory):
     if filename.endswith(roadRoot):
         #Do things, this is a good file!!
-        parsed = BeautifulSoup(open(directory+filename), "lxml")
+        parsed = BeautifulSoup(open(directory+filename), 'lxml')
         headerRow = parsed.body.find(text=re.compile(r'LRP No')).parent.parent.parent
         newRoad = []
+        roadId = filename[:-9]
         for row in headerRow.next_siblings:
             if (row.name == 'tr'):
                 cells = row.contents
 
                 newEntry = RoadEntry()
+                newEntry.roadId = roadId
                 newEntry.lrpNumber = cells[3].text.strip()
                 try:
                     newEntry.roadChainage = float(cells[5].text.strip())
                 except ValueError:
-                    print("Couldn't convert chainage to float for file {}: {}".format(filename,cells[5].text.strip()))
+                    print('Couldn\'t convert chainage to float for road {}. Ignoring row.'.format(roadId))
                     break
 
                 newEntry.lrpType = cells[7].text.strip()
@@ -42,12 +45,12 @@ for filename in os.listdir(directory):
                 try:
                     newEntry.latitude = float(cells[11].text.strip())
                 except ValueError:
-                    print("Couldn't convert latitude to float for file {}: {}".format(filename,cells[11].text.strip()))
+                    print('Couldn\'t convert latitude to float for road {}. Ignoring row.'.format(roadId))
                     break
                 try:
                     newEntry.longitude = float(cells[13].text.strip())
                 except ValueError:
-                    print("Couldn't convert longitude to float for file {}: {}".format(filename,cells[13].text.strip()))
+                    print('Couldn\'t convert longitude to float for road {}. Ignoring row.'.format(roadId))
                     break
 
                 newRoad.append(newEntry)
@@ -59,15 +62,15 @@ for filename in os.listdir(directory):
         csvFile = filename.replace(' ', '')[:-4].upper()
         with open('../RMMS/CSV/'+csvFile+'.csv', 'w') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(("LRP Number", "Road Chainage","Type","Desc","Latitutde","Longitude"))
-            writer.writerows((entry.lrpNumber,entry.roadChainage,entry.lrpType,entry.desc,entry.latitude,entry.longitude) for entry in newRoad)
+            writer.writerow(('Road Id','LRP Number', 'Road Chainage','Type','Desc','Latitude','Longitude'))
+            writer.writerows((entry.roadId,entry.lrpNumber,entry.roadChainage,entry.lrpType,entry.desc,entry.latitude,entry.longitude) for entry in newRoad)
 
-print("Writing to summary file!")
+print('Writing to summary file!')
 
 with open('../RMMS/CSV/_allRoads.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(("LRP Number","Road Chainage","Type","Desc","Latitutde","Longitude"))
+    writer.writerow(('Road Id','LRP Number','Road Chainage','Type','Desc','Latitude','Longitude'))
     for road in allRoads:
-        writer.writerows((entry.lrpNumber,entry.roadChainage,entry.lrpType,entry.desc,entry.latitude,entry.longitude) for entry in road)
+        writer.writerows((entry.roadId,entry.lrpNumber,entry.roadChainage,entry.lrpType,entry.desc,entry.latitude,entry.longitude) for entry in road)
 
-print("All done. Thanks for playing")
+print('All done. Thanks for playing.')
