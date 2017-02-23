3f01afad2fd3c8e03b211a04631304bc9c4b09da
Erin Bartholomew <erin.bartholomew@pointsource.com>
2017-02-23 13:57:12 +0100

    Addition of fixing coordinates to superfine

--- DataCleaning/CleanData.py
+++ DataCleaning/CleanData.py
@@ -1,13 +1,28 @@
 import sys
 
 import ParseHTM
-import Smoothing
+import MatchingRoadBridge
+import CoordinateFixing
 
+#First and optionally, parse the HTM data into CSV files ready for processing
+#This is optional and triggered by a flag on calling because it takes some time
 shouldConvertData = '--parse-data'
 if len(sys.argv)>1 and sys.argv[1] == shouldConvertData:
     ParseHTM.parseRoadInfo()
     ParseHTM.parseBridgeInfo()
 
+#Fix potential swaps of latitude and longitude in data
+CoordinateFixing.fixRoads()
+CoordianteFixing.fixBridges()
 
+#Smooth road data to reduce effects of outlying points in a road due to bad data
+
+
+#Address data sets where one of two sets of latitude/longitude are missing
+
+
+#Finally, remove all roads and bridges that are outside the Bangladesh boundary
+CoordinateFixing.boundCheckRoads()
+CoordinateFixing.boundCheckBridges()
 
 print('Done with data cleaning.')
