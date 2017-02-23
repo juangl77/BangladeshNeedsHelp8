7ffdc3608c2df60012e3e2b4e4b611aa819a98f2
cynthanijmeijer <cynthanijm@hotmail.com>
2017-02-23 12:31:32 +0100

    Coordinate fixing
    Switching & Boundary

--- DataCleaning/CoordinateFixing.py
+++ DataCleaning/CoordinateFixing.py
@@ -1,13 +1,11 @@
 import csv
-
+import shapefile
 
 class Coordinate():
- lat = 0
- lon = 0
 	
  def __init__(self,lat,lon):
-  self.lat = lat
-  self.lon = lon
+  self.lat = float(lat)
+  self.lon = float(lon)
 
  def __str__(self):
   return "("+str(self.lat)+", "+str(self.lon)+")"
@@ -19,9 +17,11 @@ class Coordinate():
    return Coordinate(self.lon,self.lat)
 
 class Row():
+
  def __init__(self,row):
   self.coor = Coordinate(row['Latitude'],row['Longitude'])
   self.data = row
+
  def fix(self):
   fixed_coor = self.coor.fix()
   fixed_data = self.data
@@ -29,27 +29,69 @@ class Row():
   fixed_data['Longitude'] = fixed_coor.lon
   return Row(fixed_data)
   
-			
+class Boundary():
+ def __init__(self,filename):
+  self.box = shapefile.Reader(filename).shapes()[0].bbox
+  
+ def inside(self,coordinate):
+  return ((coordinate.lat >= self.box[1] and coordinate.lat <= self.box[3]) and (coordinate.lon >= self.box[0] and coordinate.lon <= self.box[2]))
 
-rows = []
+def readBridges():
+ rows = []
 
-with open('../BMMS/CSV/_allBridges.csv') as csvfile:
- reader = csv.DictReader(csvfile)
- for row in reader:
-  rows.append(Row(row))
+ with open('../BMMS/CSV/_allBridges.csv') as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+   rows.append(Row(row))
 
-print(len(rows))
-		
-new_rows = map(lambda r: r.fix(), rows)
+ return rows
+ 
+def writeBridges(rows,filename):
+ with open(filename, 'w') as fixed_csvfile:
+  fieldnames = ['Number','Structure Name','Condition of Bridge', 'TotalWidth','TotalLength','ConstructionYear',
+   'NumberOfSpan','Zone','Circle','Division','Sub-Division','RoadNo','BridgeNo','StructureLRPName','ReferenceLRPNo','LocationLRPOffset',
+   'LocationChainage','Latitude','Longitude']
+  writer = csv.DictWriter(fixed_csvfile, fieldnames = fieldnames, dialect='excel')
+  writer.writeheader()
+  for row in rows:
+   writer.writerow(row.data)
+   
+def readRoads():
+ rows = []
 
-with open('../BMMS/CSV/_allBridges_fixed.csv', 'w') as fixed_csvfile:
- fieldnames = ['Number','Structure Name','Condition of Bridge', 'TotalWidth','TotalLength','ConstructionYear',
-  'NumberOfSpan','Zone','Circle','Division','Sub-Division','RoadNo','BridgeNo','StructureLRPName','ReferenceLRPNo','LocationLRPOffset',
-  'LocationChainage','Latitude','Longitude']
- writer = csv.DictWriter(fixed_csvfile, fieldnames = fieldnames, dialect='excel')
- writer.writeheader()
- for row in new_rows:
-  writer.writerow(row.data)
+ with open('../RMMS/CSV/_allRoads.csv') as csvfile:
+  reader = csv.DictReader(csvfile)
+  for row in reader:
+   rows.append(Row(row))
 
+ return rows  
+ 
+def writeRoads(rows,filename):
+ with open(filename, 'w') as fixed_csvfile:
+  fieldnames = ['Road Id','LRP Number','Road Chainage','Type','Desc','Latitude','Longitude']
+  writer = csv.DictWriter(fixed_csvfile, fieldnames = fieldnames, dialect='excel')
+  writer.writeheader()
+  for row in rows:
+   writer.writerow(row.data)
+ 
+def fixBridges():
+ rows = readBridges()		
+ new_rows = map(lambda r: r.fix(), rows)
+ writeBridges(new_rows,'../BMMS/CSV/_allBridges_fixed.csv')
 
+def fixRoads():
+ rows = readRoads()		
+ new_rows = map(lambda r: r.fix(), rows)
+ writeRoads(new_rows,'../RMMS/CSV/_allRoads_fixed.csv')
 
+def boundCheckBridges():
+ bound = Boundary('../WBSIM/gis/gadm/BGD_adm0.shp')
+ rows = readBridges()
+ new_rows = filter(lambda r: bound.inside(r.coor), rows)
+ writeBridges(new_rows,'../BMMS/CSV/_allBridges_final.csv')
+ 
+def boundCheckRoads():
+ bound = Boundary('../WBSIM/gis/gadm/BGD_adm0.shp')
+ rows = readRoads()
+ new_rows = filter(lambda r: bound.inside(r.coor), rows)
+ writeRoads(new_rows,'../RMMS/CSV/_allRoads_final.csv')
