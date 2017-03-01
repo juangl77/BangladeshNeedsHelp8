4e5c01a29e97945b5ce347d4dd9042beaf004031
Tony Reijm <tbreijm@gmail.com>
2017-03-01 13:38:42 +0100

    Set up for Writing Objects to the XLSX file

---
+++ A2ComponentBuilding/BridgesScript.py
@@ -0,0 +1,17 @@
+from ExcelWriter import ExcelWriter
+from SimioObject import TruckObject, ChittagongObject, DhakaObject, BridgeObject
+from Location import Location
+import ColumnMapping
+
+ColumnMapping.init()
+
+writer = ExcelWriter("bridges.xlsx")
+
+objects = [
+	TruckObject(Location(0,0,0), 48),
+	ChittagongObject(Location(10,0,10), 5),
+	DhakaObject(Location(20,0,20)),
+	BridgeObject("Bridge", Location(15,0,15), "B", 75)
+]
+
+writer.write(objects, [], [])
