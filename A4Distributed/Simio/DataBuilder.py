1b8de2a1d2b48ae08f55fa72230278b5d0020629
eebart <eebartholomew@gmail.com>
2017-03-30 10:46:03 +0200

    math updates to traffic arrivals, exporting csvs

--- A4Distributed/Simio/DataBuilder.py
+++ A4Distributed/Simio/DataBuilder.py
@@ -11,7 +11,7 @@ from BridgeIndexer import BridgeIndexer
 from DataReader import RoadData, BridgeData, TrafficData, Traffic
 
 class DataBuilder():
-	scalingFactor = 100 # TODO experiment with this number
+	scalingFactor = 1 # TODO experiment with this number
 	trafficChangeThreshold = 0.1
 
 	def __init__(self, roadData, index, trafficData):
@@ -50,15 +50,20 @@ class DataBuilder():
 				if originalTrafficIndex != currentTrafficIndex :
 					originalTraffic = self.trafficData[originalTrafficIndex]
 					newTraffic = self.trafficData[currentTrafficIndex]
+					print(dataPoint.lrp)
 					if abs(originalTraffic.leftTraffic.bus-newTraffic.leftTraffic.bus) > self.trafficChangeThreshold*originalTraffic.leftTraffic.bus:
+						print('Bus change in traffic {} -> {}'.format(originalTraffic.leftTraffic.bus,newTraffic.leftTraffic.bus))
 						busChange = True
 						trafficChange.bus = newTraffic.leftTraffic.bus-originalTraffic.leftTraffic.bus
 					if abs(originalTraffic.leftTraffic.truck-newTraffic.leftTraffic.truck) > self.trafficChangeThreshold*originalTraffic.leftTraffic.truck:
+						print('Truck change in traffic {} -> {}'.format(originalTraffic.leftTraffic.truck,newTraffic.leftTraffic.truck))
 						truckChange = True
 						trafficChange.truck = newTraffic.leftTraffic.truck-originalTraffic.leftTraffic.truck
 					if abs(originalTraffic.leftTraffic.passenger-newTraffic.leftTraffic.passenger) > self.trafficChangeThreshold*originalTraffic.leftTraffic.passenger:
+						print('Passenger change in traffic {} -> {}'.format(originalTraffic.leftTraffic.passenger,newTraffic.leftTraffic.passenger))
 						passengerChange = True
 						trafficChange.passenger = newTraffic.leftTraffic.passenger-originalTraffic.leftTraffic.passenger
+					print()
 
 				if dataPoint.gap == "BS":
 					startNodeData = dataPoint
